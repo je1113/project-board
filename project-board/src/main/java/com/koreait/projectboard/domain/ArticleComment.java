12cf4f9859cc6239b58c3829a62127c961b99312
jooeun <jje320594@gmail.com>
2022-12-28 11:24:55 +0900

    #5domain: gitflow-feature-stash: #6LogicTest
    Article ArticleComment 세팅

--- project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
+++ project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
@@ -1,10 +1,27 @@
 package com.koreait.projectboard.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class ArticleComment {
-    private long id;
-    private long article;   //게시글 (id)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Setter @ManyToOne(optional = false) private Article article;   //게시글 (id)
     private String content;
     private LocalDateTime createdAt;
     private String createdBy;
