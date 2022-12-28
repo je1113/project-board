905931cc17e1561eaa7708df6fe1a10315bb9b0b
jooeun <jje320594@gmail.com>
2022-12-28 11:41:29 +0900

    spring data jpa 속성과 설정
    * jpa auditing 기능을 쓰기 위해 jpaConfig를추가
    * db접근과 개발 편의를 위한 설정

--- project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
+++ project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
@@ -3,10 +3,15 @@ package com.koreait.projectboard.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @ToString
@@ -22,9 +27,30 @@ public class ArticleComment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Setter @ManyToOne(optional = false) private Article article;   //게시글 (id)
-    private String content;
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @Setter @Column(nullable = false, length = 500) private String content;
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false)private String createdBy;
+    @LastModifiedDate @Column(nullable = false)private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column(nullable = false)private String modifiedBy;
+    protected ArticleComment(){}
+    protected ArticleComment(Article article,String content){
+        this.article=article;
+        this.content=content;
+    }
+
+    public static ArticleComment of(Article article, String content){
+        return new ArticleComment(article, content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(!(obj instanceof ArticleComment articleComment)) return false;
+        return id!=null && id.equals(articleComment.id);
+    }
 }
